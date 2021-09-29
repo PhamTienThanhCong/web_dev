a303e242b30bfdc3dc757a89db26e75767ce7e2f
congphamtienthanh <cong.pttc@gmail.com>
2021-09-29 21:21:39 +0700

    luyen tap

--- node js/hello_world/index.js
+++ node js/hello_world/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
 
 app.get('/hello', (req, res) => {
   res.send('<h1>Hello world!</h1><br><a href="http://localhost:3000/next">hello world</a>')
