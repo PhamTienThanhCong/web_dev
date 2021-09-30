eb29c5c99c21000d1f1a840855cf71be93c941cf
congphamtienthanh <cong.pttc@gmail.com>
2021-09-30 21:33:13 +0700

    đang lỗi mai làm tiếp

---
+++ node js/luyen_tap_hello_world/src/index.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
+const morgan = require('morgan')
+const handlebars = require('express-handlebars');
+
+app.use(morgan('combined'))
+
+// app.engine('handlebars', handlebars({
+//   extname = '.handlebars'
+// }));
+
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'rescources\\views'));
+
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/', (req, res) => {
+  res.render('test')
+})
+
+app.get('/luyen-tap', (req, res) => {
+  res.send(`
+    <h3> hello world </h3>
+  `)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
