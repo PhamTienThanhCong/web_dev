cf430a4455f2975f074ebd4822e2d625958d6a29
congphamtienthanh <cong.pttc@gmail.com>
2021-10-01 16:17:05 +0700

    đổi tên express Handlebars

--- node js/expressHandlebars/src/index.js
+++ node js/expressHandlebars/src/index.js
@@ -7,17 +7,12 @@ const handlebars = require('express-handlebars');
 
 app.use(morgan('combined'))
 
-// app.engine('handlebars', handlebars({
-//   extname = '.handlebars'
-// }));
-
 app.engine('hbs', handlebars({
   extname: '.hbs'
 }))
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'rescources\\views'));
-
-
 
 app.get('/', (req, res) => {
   res.render('home')
