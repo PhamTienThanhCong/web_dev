700dc417ecd4c7d4c1edd323a23f0dd1679a5841
congphamtienthanh <cong.pttc@gmail.com>
2021-10-02 23:27:30 +0700

    login and check tét

--- node js/next/src/index.js
+++ node js/next/src/index.js
@@ -3,9 +3,11 @@ const app         = express()
 const port        = 3000
 const morgan      = require('morgan')
 const handlebars  = require('express-handlebars');
-const path        = require('path')
+const path        = require('path');
+const { Console } = require('console');
+var test;
 
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 app.engine('hbs', handlebars({
     extname: '.hbs'
@@ -15,8 +17,28 @@ app.set('views', path.join(__dirname,'scrouce/views'));
 
 app.use(express.static(path.join(__dirname,'public')))
 
+
 app.get('/', (req, res) => {
+  
+  if (req.query.email == "cong.pttc@gmail.com" && req.query.passworld == "cong" && req.query.check == "on"){
+    res.render('home')
+  }
+  else{
+    console.log("lỗi cú pháp")
+  }
+  res.render('login')
+})
+
+app.get('/home', (req, res) => {
+  test = req.query.search
+  if (test=="news"){
+    res.render('news')
+  }
   res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(port, () => {
