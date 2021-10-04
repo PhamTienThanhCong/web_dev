fc173748a0e5b78610f31596f919c7d1289b8bdf
congphamtienthanh <cong.pttc@gmail.com>
2021-10-04 15:53:51 +0700

    đổi tên file theo thứ tự

---
+++ node js/4_next/src/index.js
@@ -0,0 +1,44 @@
+const express     = require('express')
+const app         = express()
+const port        = 3000
+const morgan      = require('morgan')
+const handlebars  = require('express-handlebars');
+const path        = require('path');
+const { Console } = require('console');
+var test;
+
+// app.use(morgan('combined'))
+
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'scrouce/views'));
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('login')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.render('home')
+})
+
+app.get('/home', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
