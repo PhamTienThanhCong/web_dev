0de4243d96132e027402efc074895f565dd48514
congphamtienthanh <cong.pttc@gmail.com>
2021-11-23 09:20:58 +0700

    like post

--- reactjs/tiktok/src/App.js
+++ reactjs/tiktok/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+let statusLike = 'like';
+let yourStatusLike = '';
 
 function App() {
+  const [number, setNumber] = useState(1)
+  
+  const changeNumber = () =>{
+    if (number%2 === 1){
+      statusLike = 'đã like'
+      yourStatusLike = 'Bạn và'
+    }
+    else{
+      statusLike = 'like'
+      yourStatusLike = ''
+    }
+    setNumber(number + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          đây là khởi tạo reactjs
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>đây là ảnh gái xinh</h2>
+      <p>{yourStatusLike} 4 người khác</p>
+      <button onClick={changeNumber}>{statusLike}</button>
+      <p>Bạn đã ấn: {number}</p>
     </div>
   );
 }
