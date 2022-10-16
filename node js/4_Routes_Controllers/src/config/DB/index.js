4e8448a9d4fe2186b045e540561e18b2ddeeb2f9
congphamtienthanh <congphamtienthanh@gmail.com>
2022-10-16 18:10:43 +0700

    oe=ke

--- node js/4_Routes_Controllers/src/config/DB/index.js
+++ node js/4_Routes_Controllers/src/config/DB/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connet() {
 
     try {
-        await mongoose.connect('mongodb://localhost:27017/f8_education');
+        await mongoose.connect('mongodb://localhost:27017/users');
         console.log('done connecting!!!')
     } catch (error) {
         console.log('fail connecting :((')
